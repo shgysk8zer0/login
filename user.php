0866534798bcddee49041d0cdb1d177f3399500d
Chris Zuber <shgysk8zer0@gmail.com>
2017-01-04 12:39:25 -0800

    Add User class
    Provides methods to serialize, unserialize, set cookies, verify
    passwords, etc.

---
+++ user.php
@@ -0,0 +1,228 @@
+<?php
+
+namespace shgysk8zer0\Login;
+
+use \shgysk8zer0\Core_API as API;
+
+use \shgysk8zer0\Core as Core;
+
+class User implements \jsonSerializable, \Serializable
+{
+	use WP_Pass;
+	use Passwords;
+
+	const MAGIC_PROPERTY = '_user_data';
+
+	const USER_TABLE     = 'users';
+
+	const DATA_TABLE     = 'user_data';
+
+	private $id         = null;
+
+	public $username    = null;
+
+	public $email       = null;
+
+	private $password   = null;
+
+	private $_pdo       = null;
+
+	private $_db_creds  = null;
+
+	private $_user_data = array();
+
+	private $_tables    = array();
+
+	public static $check_wp_pass = false;
+
+	private static $_instances = array();
+
+	final static public function load($config)
+	{
+		if (! array_key_exists($config, static::$_instances)) {
+			static::$_instances[$config] = new self($config);
+		}
+		return static::$_instances[$config];
+	}
+
+	public function __construct($creds)
+	{
+		$this->_db_creds = $creds;
+		$this->_pdo = \shgysk8zer0\Core\PDO::load($creds);
+	}
+
+	public function __invoke($user, $password)
+	{
+		$stm = $this->_pdo->prepare($this->_getQuery());
+		$stm->bindParam(':user', $user, \PDO::PARAM_STR);
+		if ($stm->execute()) {
+			$data = $stm->fetch(\PDO::FETCH_ASSOC);
+
+			if (
+				array_key_exists('password', $data)
+				and ($this->_passwordVerify($password, $data['password']))
+			) {
+				$this->_setData($data);
+				return true;
+			} elseif (
+				array_key_exists('password', $data) and static::$check_wp_pass === true
+				and ($this->_passwordVerify($password, $data['password']))
+			) {
+				$hash = $this->_passwordHash($password);
+				$update = $pdo->prepare("UPDATE `users` SET `password` = :hash WHERE `username` = :user LIMIT 1;");
+				$update->bindParam(':hash', $hash);
+				$update->bindParam(':user', $data['username']);
+				if ($update->execute()) {
+					$data['password'] = $hash;
+					$this->_setData($data);
+					return true;
+				} else {
+					trigger_error('Error updating password');
+					return false;
+				}
+			} else {
+				return false;
+			}
+		}
+		return false;
+	}
+
+	public function __get($prop)
+	{
+		if ($this->__isset($prop)) {
+			return $this->{self::MAGIC_PROPERTY}[$prop];
+		} else {
+			trigger_error("Attempting to get unknown property: '$prop'.");
+		}
+	}
+
+	public function __isset($prop)
+	{
+		return array_key_exists($prop, $this->{self::MAGIC_PROPERTY});
+	}
+
+	public function __toString()
+	{
+		return $this->username;
+	}
+
+	public function __debugInfo()
+	{
+		return $this->_getData();
+	}
+
+	public function jsonSerialize()
+	{
+		return $this->_getData();
+	}
+
+	public function serialize()
+	{
+		return serialize([
+			'username' => $this->username,
+			'tables'   => $this->_tables,
+			'db_creds' => $this->_db_creds,
+		]);
+	}
+
+	public function unserialize($data)
+	{
+		$data = unserialize($data);
+		$this->_db_creds = $data['db_creds'];
+		$this->_pdo = Core\PDO::load($this->_db_creds);
+		$this->_tables = $data['tables'];
+
+		$stm = $this->_pdo->prepare($this->_getQuery());
+		$stm->bindParam(':user', $data['username'], \PDO::PARAM_STR);
+		if ($stm->execute()) {
+			$data = $stm->fetch(\PDO::FETCH_ASSOC);
+			$this->_setData($data);
+		}
+
+		static::$_instances[$this->_db_creds] = $this;
+	}
+
+	public function hasTable($table)
+	{
+		return in_array($table, $this->_tables);
+	}
+
+	public function getTables()
+	{
+		return array_map(function($table)
+		{
+			return sprintf('`%s`', preg_replace('/[^\w\- ]/', null, $table));
+		}, $this->_tables);
+	}
+
+	public function addTable($table)
+	{
+		if (! $this->hasTable($table)) {
+			array_push($this->_tables, $table);
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public static function restore($key = 'user')
+	{
+		if (array_key_exists($key, $_SESSION)) {
+			return unserialize($_SESSION[$key]);
+		} elseif(array_key_exists($key, $_COOKIE)) {
+			$_SESSION[$key] = base64_decode($_COOKIE[$key]);
+			return unserialize($_SESSION[$key]);
+		} else {
+			return new self('config.json');
+		}
+	}
+
+	public function setCookie($key = 'user')
+	{
+		return \setcookie(
+			$key,
+			base64_encode(serialize($this)),
+			strtotime('+1 month'),
+			'/',
+			$_SERVER['HTTP_HOST'],
+			array_key_exists('HTTPS', $_SERVER),
+			true
+		);
+	}
+
+	public function setSession($key)
+	{
+		$_SESSION[$key] = serialize($this);
+	}
+
+	private function _getData()
+	{
+		return array_merge([
+			'id' => $this->id,
+			'username' => $this->username,
+			'email' => $this->email,
+		], $this->{self::MAGIC_PROPERTY});
+	}
+
+	private function _setData(Array $data)
+	{
+		$this->id       = $data['id'];
+		$this->username = $data['username'];
+		$this->email    = $data['email'];
+		$this->password = $data['password'];
+		unset($data['id']);
+		unset($data['username']);
+		unset($data['email']);
+		unset($data['password']);
+		$this->{self::MAGIC_PROPERTY} = $data;
+	}
+
+	private function _getQuery()
+	{
+		return 'SELECT * FROM `users`
+			JOIN(`user_data`, `subscribers`)
+			ON (`user_data`.`id` = `users`.`id` AND `subscribers`.`id` = `users`.`id`)
+			WHERE `users`.`email` = :user OR `users`.`username` = :user
+			LIMIT 1;';
+	}
+}
