5bdeed7bd26fbffbc669049fd35dd336d035ccef
Chris Zuber <shgysk8zer0@gmail.com>
2017-02-20 11:02:09 -0800

    Add permission checks and update documentation
    Also now requires PHP 7 due to adding typehinting and declaring return
    value types

--- user.php
+++ user.php
@@ -1,10 +1,28 @@
 <?php
-
+/**
+ * @author Chris Zuber <shgysk8zer0@gmail.com>
+ * @package shgysk8zer0\Login
+ * @version 1.0.0
+ * @copyright 2017, Chris Zuber
+ * @license http://opensource.org/licenses/GPL-3.0 GNU General Public License, version 3 (GPL-3.0)
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 namespace shgysk8zer0\Login;
 
 use \shgysk8zer0\Core_API as API;
 
-use \shgysk8zer0\Core as Core;
+use \shgysk8zer0\Core;
 
 use \shgysk8zer0\PHPCrypt as Crypto;
 
@@ -44,7 +62,14 @@ class User implements \jsonSerializable, \Serializable
 
 	public static  $expires = '+1 month';
 
-	final static public function load($config)
+	private $_permissions;
+
+	/**
+	 * Restore instance. Creates if instances doesn't exist
+	 * @param  String $config DB credentials file
+	 * @return self           New or existing instance of self
+	 */
+	final static public function load(String $config): self
 	{
 		if (! array_key_exists($config, static::$_instances)) {
 			static::$_instances[$config] = new self($config);
@@ -52,7 +77,11 @@ class User implements \jsonSerializable, \Serializable
 		return static::$_instances[$config];
 	}
 
-	public function __construct($creds = 'connect.json')
+	/**
+	 * Create a new instance from database credentials file
+	 * @param string $creds path/to/creds.json
+	 */
+	public function __construct(String $creds = Core\PDO::DEFAULT_CON)
 	{
 		$this->_db_creds = $creds;
 		$this->_pdo = \shgysk8zer0\Core\PDO::load($creds);
@@ -64,7 +93,13 @@ class User implements \jsonSerializable, \Serializable
 		}
 	}
 
-	public function __invoke($user, $password)
+	/**
+	 * Login using usrename/email and passowrd
+	 * @param  String $user     user@example.com or username
+	 * @param  String $password Password
+	 * @return Bool             Whether or not login was successful
+	 */
+	public function __invoke(String $user, String $password): Bool
 	{
 		$stm = $this->_pdo->prepare($this->_getQuery());
 		$stm->bindParam(':user', $user, \PDO::PARAM_STR);
@@ -78,11 +113,16 @@ class User implements \jsonSerializable, \Serializable
 				$this->_setData($data);
 				return true;
 			} elseif (
-				array_key_exists('password', $data) and static::$check_wp_pass === true
-				and ($this->passwordVerify($password, $data['password']))
+				static::$check_wp_pass === true and array_key_exists('password', $data)
+				and ($this->CheckPassword($password, $data['password']))
 			) {
 				$hash = $this->passwordHash($password);
-				$update = $pdo->prepare("UPDATE `users` SET `password` = :hash WHERE `username` = :user LIMIT 1;");
+				$update = $pdo->prepare(
+					'UPDATE `users`
+					SET `password` = :hash
+					WHERE `username` = :user
+					LIMIT 1;'
+				);
 				$update->bindParam(':hash', $hash);
 				$update->bindParam(':user', $data['username']);
 				if ($update->execute()) {
@@ -100,38 +140,60 @@ class User implements \jsonSerializable, \Serializable
 		return false;
 	}
 
-	public function __get($prop)
+	/**
+	 * Magic getter
+	 * @param  String $prop Name of property
+	 * @return Mixed        Its value
+	 */
+	public function __get(String $prop)
 	{
-		if ($this->__isset($prop)) {
-			return $this->{self::MAGIC_PROPERTY}[$prop];
-		} else {
-			trigger_error("Attempting to get unknown property: '$prop'.");
-		}
+		return $this->{self::MAGIC_PROPERTY}[$prop] ?? null;
 	}
 
-	public function __isset($prop)
+	/**
+	 * Magic isset method
+	 * @param  String  $prop Name of property
+	 * @return boolean       Whether or not it is set
+	 */
+	public function __isset(String $prop): Bool
 	{
 		return array_key_exists($prop, $this->{self::MAGIC_PROPERTY});
 	}
 
-	public function __toString()
+	/**
+	 * Method used to convert class object to string
+	 * @return string Username
+	 */
+	public function __toString(): String
 	{
 		return $this->username;
 	}
 
-	public function __debugInfo()
+	/**
+	 * Method used when calling debugging functions, such as `var_dump`
+	 * @return Array Data array
+	 */
+	public function __debugInfo(): Array
 	{
 		return $this->_getData();
 	}
 
-	public function jsonSerialize()
+	/**
+	 * Method used when converting class object to JSON using `json_encode`
+	 * @return Array Data array
+	 */
+	public function jsonSerialize(): Array
 	{
 		return $this->_getData();
 	}
 
-	public function serialize()
+	/**
+	 * Class `serialize` method
+	 * @return String Serialized class data
+	 */
+	public function serialize(): String
 	{
-		$expires = strtotime(static::$expires);
+		$expires = is_string(static::$expires) ? strtotime(static::$expires) : static::$expires;
 		$data = [
 			'username' => $this->username,
 			'tables'   => $this->_tables,
@@ -141,6 +203,11 @@ class User implements \jsonSerializable, \Serializable
 		return serialize($data);
 	}
 
+	/**
+	 * Method used to `unserialize`
+	 * @param  String $data Serialized data
+	 * @return void
+	 */
 	public function unserialize($data)
 	{
 		$data = @unserialize($data);
@@ -161,46 +228,84 @@ class User implements \jsonSerializable, \Serializable
 				$this->logout();
 			}
 
+
 			static::$_instances[$this->_db_creds] = $this;
 		}
 	}
 
-	public function setCookie($key = self::KEY, $crypto_pwd = null)
+	/**
+	 * Check if a user has permission for a given action
+	 * @param  String $permission Permission being checked
+	 * @return Bool               Whether or not user has permission
+	 */
+	public function hasPermission(String $permission): Bool
+	{
+		return $this->_permissions->{$permission} === '1' ?? false;
+	}
+
+	/**
+	 * Sets login cookie, optionally using encryption
+	 * @param  String $key        Sets `$_COOKIE[$key]`
+	 * @param  String $crypto_pwd Optional password to encrypt with
+	 * @return self               Return self to make chainable
+	 */
+	public function setCookie(String $key = self::KEY, String $crypto_pwd = null): self
 	{
 		if (is_string($crypto_pwd)) {
 			$cookie = static::encrypt(@serialize($this), $crypto_pwd);
 		} else {
 			$cookie = base64_encode(@serialize($this));
 		}
-		static::_cookie($key, $cookie, $this::$expires);
+		static::_cookie($key, $cookie, static::$expires);
 		return $this;
 	}
 
-	public function setSession($key = self::KEY)
+	/**
+	 * Save to session
+	 * @param  String $key Sets `$_SESSION[$key]`
+	 * @return self        Return self to make chainable
+	 */
+	public function setSession(String $key = self::KEY): self
 	{
 		$_SESSION[$key] = @serialize($this);
 		return $this;
 	}
 
-	public function logout($key = self::KEY)
+	/**
+	 * Logs a user out, clearing data
+	 * @param  String $key `$_COOKIE[$key]` and `$_SESSION[$key]`
+	 * @return self        Return self to make chainable
+	 */
+	public function logout(String $key = self::KEY): self
 	{
 		if (array_key_exists($key, $_COOKIE)) {
 			static::_cookie($key, null, 1);
 		}
-		unset($_SESSION[$key]);
+		if (session_status() === PHP_SESSION_ACTIVE) {
+			unset($_SESSION[$key]);
+		}
 		$this->id = null;
 		$this->username = null;
 		$this->password = null;
-		$this->_user_data = array();
+		$this->{self::MAGIC_PROPERTY} = array();
 		return $this;
 	}
 
-	public function hasTable($table)
+	/**
+	 * Checks if a MySQL table is set to be used for setting user data
+	 * @param  String $table Name of table
+	 * @return Bool          Whether or not is has already been added
+	 */
+	public function hasTable(String $table): Bool
 	{
 		return in_array($table, $this->_tables);
 	}
 
-	public function getTables()
+	/**
+	 * Retrieves a list of all extra MySQL tables used for user data
+	 * @return Array [table1, ...]
+	 */
+	public function getTables(): Array
 	{
 		return array_map(function($table)
 		{
@@ -208,7 +313,12 @@ class User implements \jsonSerializable, \Serializable
 		}, $this->_tables);
 	}
 
-	public function addTable($table)
+	/**
+	 * Adds a MySQL table to the list of tables used for user data
+	 * @param  String $table Name of table
+	 * @return Bool          Whether or not it was appended to the list
+	 */
+	public function addTable(String $table): Bool
 	{
 		if (! $this->hasTable($table)) {
 			array_push($this->_tables, $table);
@@ -218,7 +328,18 @@ class User implements \jsonSerializable, \Serializable
 		}
 	}
 
-	public static function restore($key = self::KEY, $db_creds = null, $crypto_pwd = null)
+	/**
+	 * Restore login from `$_COOKIE` or `$_SESSION`
+	 * @param  String $key        Name of key in `$_COOKIE` or `$_SESSION`
+	 * @param  String $db_creds   path/to/db_creds.json
+	 * @param  String $crypto_pwd Optional password to use to decrypt login cookie
+	 * @return self               The restored and possibly logged in user
+	 */
+	public static function restore(
+		String $key        = self::KEY,
+		String $db_creds   = Core\PDO::DEFAULT_CON,
+		String $crypto_pwd = null
+	): self
 	{
 		try {
 			if (!is_string($db_creds)) {
@@ -229,7 +350,7 @@ class User implements \jsonSerializable, \Serializable
 				$user = @unserialize($_SESSION[$key]);
 				if (!$user) {
 					unset($_SESSION[$key]);
-					throw new \Exception('Unable to restore from session.');
+					throw new \RuntimeException('Unable to restore from session.');
 				}
 			} elseif (array_key_exists($key, $_COOKIE)) {
 				if (is_string($crypto_pwd)) {
@@ -240,20 +361,21 @@ class User implements \jsonSerializable, \Serializable
 				if (!$user) {
 					static::_cookie($key, null, 1);
 					unset($_COOKIE[$key]);
-					throw new \Exception('Unable to restore from cookie.');
+					throw new \RuntimeException('Unable to restore from cookie.');
 				} elseif (session_status() === PHP_SESSION_ACTIVE) {
 					$_SESSION[$key] = @serialize($user);
 				}
 			} else {
 				$user = new self($db_creds);
 			}
-		} catch (\Exception $e) {
-			trigger_error($e->getMessage());
-		} catch (\Error $e) {
+		} catch (\Throwable $e) {
 			trigger_error($e->getMessage());
 		} finally {
-			if (is_object($user) and $user instanceof self and !static::_isExpired($user::$expires)) {
+			if (isset($user) and is_object($user) and $user instanceof self) {
 				static::$_instances[$db_creds] = $user;
+				if (static::_isExpired($user::$expires)) {
+					$user->logout();
+				}
 			} else {
 				$user = static::$_instances[$db_creds] = new self($db_creds);
 				if (session_status() === PHP_SESSION_ACTIVE) {
@@ -264,7 +386,14 @@ class User implements \jsonSerializable, \Serializable
 		}
 	}
 
-	private static function _cookie($key, $value = null, $expires = 1)
+	/**
+	 * Private method for setting cookies more easily
+	 * @param  String  $key     Cookie name
+	 * @param  String  $value   Value to set it to
+	 * @param  Mixed   $expires Expiration timestamp
+	 * @return Bool             Whether or not the cookie was set
+	 */
+	private static function _cookie(String $key, String $value = null, $expires = 1): Bool
 	{
 		if (isset($value)) {
 			$_COOKIE[$key] = $value;
@@ -283,19 +412,21 @@ class User implements \jsonSerializable, \Serializable
 		);
 	}
 
-	private static function _isExpired($time)
+	/**
+	 * Does timestamp comparision
+	 * @param  Int   $expires Expiration timestamp
+	 * @return Bool           Whether or not it is less than current time
+	 */
+	private static function _isExpired(Int $expires): Bool
 	{
-		$now = new \DateTime('now');
-		if (strval(@intval($time)) == $time) {
-			return new \DateTime("@{$time}") < $now;
-		} elseif (is_string($time)) {
-			return new \DateTime($time) < $now;
-		} else {
-			return true;
-		}
+		return $expires < time();
 	}
 
-	private function _getData()
+	/**
+	 * Private method for retrieving data
+	 * @return Array Data to be shared, such as for `jsonSerialize`
+	 */
+	private function _getData(): Array
 	{
 		return array_merge([
 			'id'       => $this->id,
@@ -304,25 +435,53 @@ class User implements \jsonSerializable, \Serializable
 		], $this->{self::MAGIC_PROPERTY});
 	}
 
+	/**
+	 * Private method for setting data
+	 * @param Array $data Data, such as from a SQL query
+	 */
 	private function _setData(Array $data)
 	{
 		$this->id       = $data['id'];
 		$this->username = $data['username'];
 		$this->email    = $data['email'];
 		$this->password = $data['password'];
-		unset($data['id']);
-		unset($data['username']);
-		unset($data['email']);
-		unset($data['password']);
+		unset($data['id'] ,$data['username'], $data['email'], $data['password']);
+		$this->_permissions = $this->_getPermsissions($data['status']);
 		$this->{self::MAGIC_PROPERTY} = $data;
 	}
 
-	private function _getQuery()
+	/**
+	 * Retrieves permissions from `permissions` table and returns as object
+	 * @param  Int       $role_id `subscribers`.`status`
+	 * @return stdClass           {"$permissionName": "1" || "0", ...}
+	 */
+	private function _getPermsissions(Int $role_id): \stdClass
+	{
+		$stm = $this->_pdo->prepare(
+			'SELECT * FROM `permissions`
+			WHERE `id` = :role LIMIT 1;'
+		);
+		$stm->bindParam(':role', $role_id);
+		$stm->execute();
+		$permissions = $stm->fetchObject() ?? new \stdClass();
+		unset($permissions->id, $permissions->roleName);
+		return $permissions;
+	}
+
+	/**
+	 * Returns SQL for use in perpared statement for logging in
+	 * @return String SELECT statement
+	 */
+	private function _getQuery(): String
 	{
 		return 'SELECT * FROM `users`
-			JOIN(`user_data`, `subscribers`)
-			ON (`user_data`.`id` = `users`.`id` AND `subscribers`.`id` = `users`.`id`)
-			WHERE `users`.`email` = :user OR `users`.`username` = :user
-			LIMIT 1;';
+		JOIN (
+			`user_data`,
+			`subscribers`
+		) ON (
+			`user_data`.`id` = `users`.`id`
+			AND `subscribers`.`id` = `users`.`id`
+		)
+		LIMIT 1;';
 	}
 }
